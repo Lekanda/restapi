1d60750be5e7b058a93b9979058004105b913286
lekanda <abernaolao@gmail.com>
2020-10-27 20:34:34 +0100

    End pòint y controller para nuevos usuarios

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,11 +1,22 @@
+const Usuarios = require('../models/Usuarios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
-
-exports.registrarUsuario = () => {
-
+exports.registrarUsuario = async (req,res) => {
+    // Leer los datos del usuario y colocarlos en Usuarios
+    const usuario = new Usuarios(req.body);
+    usuario.password = await bcrypt.hash(req.body.password, 12);
+    try {
+        await usuario.save();
+        res.json({mensaje : 'Usuario nuevo guardado'});
+    } catch (error) {
+        console.log(error);  
+        res.json({mensaje : 'Hubo un error'});
+    }
 }
-
 
 exports.autenticarUsuario = () => {
 
+    
 }
