87b9fc4763380dc2111aef7707fee9c1849ecc14
lekanda <abernaolao@gmail.com>
2020-10-27 22:04:53 +0100

    Autenticar Usuario Y generar JWT

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -16,7 +16,34 @@ exports.registrarUsuario = async (req,res) => {
     }
 }
 
-exports.autenticarUsuario = () => {
+exports.autenticarUsuario = async (req,res,next) => {
+    // Buscar el usuario
+    const {email, password} = req.body
+    const usuario = await Usuarios.findOne({ email })
+    if(!usuario) {
+        // Sí no existe (401= No estas autorizado)
+        await res.status(401).json({mensaje: 'Usuario no existe'});
+        next();
+    }else{
+        // El usuario existe; Verificar si PASS es OK o NOK
+        if(!bcrypt.compareSync(password, usuario.password)) {
+            // Sí el password es incorrecto
+            await res.status(401).json({ mensaje : 'La contraseña es incorrecta' });
+            next();
+        } else {
+            // Sí el password es correcto, firmar el token
+            const token = jwt.sign({
+                email : usuario.email,
+                nombre :usuario.nombre,
+                _id : usuario._id
 
-    
+            }, 'LLAVESECRETA',
+            {
+                expiresIn : '1h'
+            });
+
+            // Retornar el Token
+            res.json({ token });
+        }
+    }
 }
